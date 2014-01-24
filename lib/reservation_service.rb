03c153480c383c0213cd05d8f99c9d1d4de04ac8
Holger Schmeisky <holger.s@fu-berlin.de>
2014-01-24 19:39:00 +0100

    changed tests for refactoring

--- lib/reservation_service.rb
+++ lib/reservation_service.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/store')
+require 'store'
 
 class ReservationService
 
@@ -8,18 +8,18 @@ class ReservationService
 
   def reserve(num_seats)
     old_seats = seats
-    
+
     newly_reserved_seats = []
     num_seats.times do |i|
       newly_reserved_seats << (old_seats.last.to_i + 1 + i)
     end
 
-    @store.write(old_seats + newly_reserved_seats) # was ist der rückgabewert?
+    @store.write(old_seats + newly_reserved_seats)
     newly_reserved_seats
   end
 
-  def list_reserved
-    seats
+  def list_reserved(limit)
+    seats.slice(0, limit)
   end
 
   def reset
